8ccb71716442569526e8154683e2b88e1467e27d
Ivan-1990 <60809865+Ivan-1990@users.noreply.github.com>
2021-12-28 19:37:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+"use sctrict"  
+
+//let number = 4.6;
+//console.log(213);
+//const persone = "Alex";
+/*const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+console.log(obj.name);
+console.log(obj['name']); 
+let arr = ['plum.png','orange.png','apple.png', 6, {}, []];
+console.log(arr[1]); */
+//alert('Hello!');
+//const result = confirm("Are you hare?")
+//console.log(result);
+//const answer = +prompt("Вам есть 18?", "18");
+//console.log(answer +5);
+//const answers = [];
+//answers[0] = prompt('Как ваше имя?', '');
+//answers[1] = prompt('Как ваше фамилия?', '');
+//answers[2] = prompt('Сколько вам лет?', '');
+//console.log(typeof(answers));
+//const category = 'toys';
+//console.log(`https://someurl.com/${category}`);
+//const user = 'Ivan';
+//alert(`Привет,${user}`);
+//console.log(4 + +"5");
+//let incr = 10,
+   // decr = 10; 
+//++incr;
+//--decr; 
+//console.log(++incr); 
+//console.log(--decr); 
+//console.log(6%2);
+//console.log(2 + 4 * 2 !== '6'); 
+//const isChecked = false,
+      //isClose = false;
+//console.log(isChecked || !isClose);
+//Задание 
+
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+    for (let i = 1; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ""), 
+            b = +prompt('На сколько оцените его?', ""); 
+            if (a.length == 0 && a == false && a.length < 50) {
+                return; 
+    } 
+    personalMovieDB.movies[a] = b;
+
+    }
+
+
+
+
+
+
+console.log(personalMovieDB);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
